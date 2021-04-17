278c35f097e2a7607ab17a0076b0fff5ad3cbac7
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-04-17 19:00:51 +0530

    updated

--- lib/screens/home/catogeries/catogery_tile.dart
+++ lib/screens/home/catogeries/catogery_tile.dart
@@ -37,7 +37,22 @@ class ImageAdnDataDislpay extends StatelessWidget {
           );
         } else {
           Navigator.push(
-              context, MaterialPageRoute(builder: (context) => NoConnection()));
+              context, PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) =>
+                    NoConnection(),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  var begin = Offset(1.0, 0.0);
+                  var end = Offset(0.0, 0.0);
+                  var curve = Curves.ease;
+
+                  var tween = Tween(begin: begin, end: end)
+                      .chain(CurveTween(curve: curve));
+                  return SlideTransition(
+                    position: animation.drive(tween),
+                    child: child,
+                  );
+                }));
         }
       },
       child: FittedBox(
